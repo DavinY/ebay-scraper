0beb8ec61ca900032c16b0d3af2c86174f634f84
Davin Y <davin@Davins-MacBook-Pro.local>
2022-03-07 12:06:44 +0700

    modified listings parser to enqueue more pages

--- parsers/listings.rb
+++ parsers/listings.rb
@@ -8,13 +8,13 @@ listings = nokogiri.css('ul.b-list__items_nofooter li.s-item')
 listings.each do |listing|
     # initialize an empty hash
     product = {}
-
+    
     # extract the information into the product hash
     product['title'] = listing.at_css('h3.s-item__title')&.text
-
+    
     # extract the price
     product['price'] = listing.at_css('.s-item__price')&.text
-
+    
     # extract the listing URL
     item_link = listing.at_css('a.s-item__link')
     product['url'] = item_link['href'] unless item_link.nil?
